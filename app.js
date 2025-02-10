267cc4562f9d48dd1b4b88ac5c74f10adba36d2c
Adiel Tec <atec94@gmail.com>
2025-02-10 14:47:06 -0600

    Actualiza app.js // Se crea un array para guardar los nombres participantes, se crea una función para agregar los nombres del input hacía el array y se crea una función que limpia el campo de texto después de agregar el elmento al array por medio del boton añadir

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+//Array declarado
+const amigos = [];
+
+//Funcion que permite agregar los nombres al array y a su vez indica un alert en caso de que se intente ingresar el campo vacio
+function agregarAmigo() {
+    var nombre_amigo=document.getElementById("amigo").value;
+    var count = amigos.push
+    if(nombre_amigo==""){
+        alert("El campo no puede estar vacio");
+    }
+
+    amigos.push(nombre_amigo);
+
+    console.log(amigos);
+
+//Permite limpiar el input del formulario despues de guardar los elementos en el array
+function limpiar(){
+
+   document.getElementById("amigo").value="";
+    }
+
+   
+
+    limpiar();
+}
+
