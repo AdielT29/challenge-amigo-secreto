44511e2611a52603380ce01d11b929e31e0e72ac
Adiel Tec <atec94@gmail.com>
2025-02-10 22:40:36 -0600

    Actualiza app.js, se agrega un metodo focus para seleccionar automaticamente el campo de texto después de agregar un nuevo elemento, de agregar un value para que al agregar un nombre el campo se limpie. Se agrega la función actualizar lista, se incluye un inner.HTML para asegurarse que no haya duplicados al actualizar, se obtienen los datos de la lista mediante un getElementById, mediante un bucle for each, se recorre el arreglo de amigos donde se encuentran los nombres y crea los elementos en una lista

--- app.js
+++ app.js
@@ -7,22 +7,38 @@ const amigos = [];
 //Funcion que permite agregar los nombres al array y a su vez indica un alert en caso de que se intente ingresar el campo vacio
 function agregarAmigo() {
     var nombre_amigo=document.getElementById("amigo").value;
-    var count = amigos.push
+   
     if(nombre_amigo==""){
         alert("El campo no puede estar vacio");
+        return;
     }
 
     amigos.push(nombre_amigo);
 
     console.log(amigos);
 
-//Permite limpiar el input del formulario despues de guardar los elementos en el array
+//Permite limpiar el input del formulario despues de guardar los elementos en el array y selecciona automáticamente el cuadro de texto
 function limpiar(){
 
    document.getElementById("amigo").value="";
+   document.getElementById("amigo").focus();
+
     }
 
-   
+//Actualiza la lista de nombres y limpia la lista antes de agregar un nuevo elemento, el bucle for each crea la lista y recorre el arreglo para agregarlos
+function actualizarLista(){
+
+    const listaAmigoul = document.getElementById("listaAmigos");
+    listaAmigoul.innerHTML = "";
+
+    amigos.forEach(amigo =>{
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigoul.appendChild(li);
+    })
+} 
+
+    actualizarLista();
 
     limpiar();
 }
